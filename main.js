22cbe92e4aef47fe9602c974b05f172d4bec9b0c
LuchoPersano <40143996+LuchoPersano@users.noreply.github.com>
2022-06-04 21:28:15 -0300

    No working page, just backup

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-// 
+document.querySelectorAll('img').forEach(image => {
+    image.addEventListener('click', () => {
+        image.toggleAttribute('fit-cover');
+        image.toggleAttribute('fit-contain');
+        console.log('Image clicked')
+    })
+})
